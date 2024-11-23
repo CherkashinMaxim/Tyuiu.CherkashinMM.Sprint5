507fee823d834cdaedd78483d1f5f5bb093eab10
CherkashinMaxim <cherkashin.maxim1@gmail.com>
2024-11-23 14:00:27 +0500

    Task0

--- Tyuiu.CherkashinMM.Sprint5.Task0.V19.Test/DataServiceTest.cs
+++ Tyuiu.CherkashinMM.Sprint5.Task0.V19.Test/DataServiceTest.cs
@@ -8,9 +8,10 @@ public class DataServiceTest
    [TestMethod] 
    public void Check()  
    {
-        string path = @"C:\Users\lans\source\repos\Tyuiu.CherkashinMM.Sprint5\Tyuiu.CherkashinMM.Sprint5.Task0.V19\bin\Debug\net8.0\OutPutFileTask0.txt";
+        string path = "..\\..\\..\\..\\Tyuiu.CherkashinMM.Sprint5.Task0.V19\\bin\\Debug\\net8.0\\OutPutFileTask0.txt";
 
         FileInfo fileInfo = new FileInfo(path);
-        Assert.AreEqual(true, fileInfo.Exists);
+        bool wait = true;
+        Assert.AreEqual(wait, fileInfo.Exists);
    } 
 } 
