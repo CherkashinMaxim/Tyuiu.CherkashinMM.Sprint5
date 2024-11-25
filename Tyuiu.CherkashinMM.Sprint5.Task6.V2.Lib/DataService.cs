81577841e38043b9bd2e17a72e6303f378fdeb47
CherkashinMaxim <cherkashin.maxim1@gmail.com>
2024-11-25 19:33:51 +0500

    Task6

--- Tyuiu.CherkashinMM.Sprint5.Task6.V2.Lib/DataService.cs
+++ Tyuiu.CherkashinMM.Sprint5.Task6.V2.Lib/DataService.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using tyuiu.cources.programming.interfaces.Sprint5; 
  
 namespace Tyuiu.CherkashinMM.Sprint5.Task6.V2.Lib;
@@ -7,7 +8,7 @@ public class DataService : ISprint5Task6V2
     public int LoadFromDataFile(string path)
     {
         string cyr_alpha = "¸éöóêåíãøùçõúôûâàïğîëäæıÿ÷ñìèòüáşÉÖÓÊÅÍÃ¨ØÙÇÕÚÔÛÂÀÏĞÎËÄÆİß×ÑÌÈÒÜÁŞ";
-        string text = File.ReadAllText(path);
+        string text = File.ReadAllText(path, Encoding.UTF8);
         int res = 0;
         foreach(char c in text)
         {
