10d182c82bb184c414199b1ec31e25ab8f7f67c3
CherkashinMaxim <cherkashin.maxim1@gmail.com>
2024-11-25 18:18:29 +0500

    Task4

--- Tyuiu.CherkashinMM.Sprint5.Task4.V25.Lib/DataService.cs
+++ Tyuiu.CherkashinMM.Sprint5.Task4.V25.Lib/DataService.cs
@@ -6,8 +6,8 @@ public class DataService : ISprint5Task4V25
 {
     public double LoadFromDataFile(string path)
     {
-        string strX = File.ReadAllText(path);
-        double x = double.Parse(strX);
+        string strX = File.ReadAllText(path).ToString();
+        double x = double.Parse(strX, System.Globalization.CultureInfo.InvariantCulture);
         double res = (Math.Pow(x, 4) + Math.Cos(x)) * Math.Sin(x);
         return Math.Round(res, 3);
     }
