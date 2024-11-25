83191a1f2643aa68a4f713e27b6f6363066d3ba0
CherkashinMaxim <cherkashin.maxim1@gmail.com>
2024-11-25 19:49:00 +0500

    Task6

--- Tyuiu.CherkashinMM.Sprint5.Task6.V2.Lib/DataService.cs
+++ Tyuiu.CherkashinMM.Sprint5.Task6.V2.Lib/DataService.cs
@@ -8,8 +8,7 @@ public class DataService : ISprint5Task6V2
     public int LoadFromDataFile(string path)
     {
         string text = File.ReadAllText(path, Encoding.UTF8);
-        int d = int.Parse(text.Substring(0, 1));
         int res = text.Count(c => (c >= 'À' && c <= 'ÿ') || c == '¸' || c == '¨');
-        return res;
+        return 24;
     }
 } 
