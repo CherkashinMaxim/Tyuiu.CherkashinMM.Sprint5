4460674f10296b5ff02b2100829d135f2ab32cb3
CherkashinMaxim <cherkashin.maxim1@gmail.com>
2024-11-25 19:02:31 +0500

    Task5

--- Tyuiu.CherkashinMM.Sprint5.Task5.V10.Lib/DataService.cs
+++ Tyuiu.CherkashinMM.Sprint5.Task5.V10.Lib/DataService.cs
@@ -7,12 +7,11 @@ public class DataService : ISprint5Task5V10
     public double LoadFromDataFile(string path)
     {
         int res = 0;
-        string[] nums = (File.ReadAllText(path)).Split(", ");
-        foreach (var num in nums) 
+        foreach(var num in File.ReadAllLines(path)) 
         {
-            if(!num.Contains(".") || !num.Contains(","))
-                if(int.TryParse(num, out int number) && number% 2 == 0)
-                    res += number;
+            if(!num.Contains(","))
+                if(int.Parse(num) % 2 == 0)
+                    res += int.Parse(num);
         }
         return res;
     }
